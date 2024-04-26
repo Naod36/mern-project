aa98b657caa868dd2d7b41ebeb1600f94bd8e95d
Naod <naodk36@gmail.com>
2024-04-26 23:37:25 +0300

    footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signin from "./pages/Signin";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Footer, FooterCopyright } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border bottom-t-8 border-teal-300">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="sm:mt-10">
+            <Link
+              to="/"
+              className=" self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-wight"
+            >
+              <div className="flex px-2 py-1  ">
+                <img src="/logo.png" className="mr-3 h-8 sm:h-9" alt="Logo" />
+                <div className=" text-black  self-center whitespace-nowrap text-2xl font-semibold dark:text-wight">
+                  Court
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6 ">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link 1 external
+                </Footer.Link>
+                <Footer.Link href="/about">Link 1 internal</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow Us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/naod36"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Link 1</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="" rel="">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#"> Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <FooterCopyright
+            href="#"
+            by="HU CEP Final project "
+            year={new Date().getFullYear()}
+            className=""
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="https://facebook.com" icon={BsFacebook} />
+            <Footer.Icon href="https://instagram.com" icon={BsInstagram} />
+            <Footer.Icon href="https://twitter.com" icon={BsTwitter} />
+            <Footer.Icon href="https://github.com/naod36" icon={BsGithub} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
