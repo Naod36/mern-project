100e1b5f36ea0199d9ce7b6b19767648bb2b91d3
Naod <naodk36@gmail.com>
2024-05-09 00:56:58 +0300

    complete case template image uplode fuction

--- client/src/pages/CreateCaseTemp.jsx
+++ client/src/pages/CreateCaseTemp.jsx
@@ -1,8 +1,58 @@
-import { FileInput, Select, TextInput, Button } from "flowbite-react";
+import { FileInput, Select, TextInput, Button, Alert } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function CreateCaseTemp() {
+  const [file, setFile] = useState(null);
+  const [imageUploadingProgress, setImageUploadingProgress] = useState(null);
+  const [imageUploadingError, setImageUploadingError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadingError("Please select an image file");
+        return;
+      }
+      setImageUploadingError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadingProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadingError("Image uploading failed.");
+          setImageUploadingProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadingProgress(null);
+            setImageUploadingError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadingError("Image upload failed.");
+      setImageUploadingProgress(null);
+      console.log(error);
+    }
+  };
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="p-5 my-7 text-3xl  font-bold">Create Case Template</h1>
@@ -23,11 +73,40 @@ export default function CreateCaseTemp() {
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-2 border-teal-500 rounded-xl p-3 border-opacity-20">
-          <FileInput type="file" accept="image/*" />
-          <Button type="button" color="dark" size="sm">
-            Upload Image
+          <FileInput
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button
+            type="button"
+            color="dark"
+            size="sm"
+            onClick={handleUploadImage}
+            disabled={imageUploadingProgress !== null}
+          >
+            {imageUploadingProgress ? (
+              <div className="w-16 h-16">
+                <CircularProgressbar
+                  value={imageUploadingProgress}
+                  text={`${imageUploadingProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              "Upload Image"
+            )}
           </Button>
         </div>
+        {imageUploadingError && (
+          <Alert color="failure">{imageUploadingError}</Alert>
+        )}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt="uploaded image"
+            className="w-full h-72 object-cover"
+          />
+        )}
         <ReactQuill
           theme="snow"
           placeholder="Write something..."
