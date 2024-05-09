041a86b3670dedda15fa7e01f7df7c2b579b0256
Naod <naodk36@gmail.com>
2024-05-09 03:14:01 +0300

    api for case template list

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,4 @@
-import Post from "../models/post.model.js";
+import Case from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
@@ -13,15 +13,57 @@ export const create = async (req, res, next) => {
     .join("-")
     .toLowerCase()
     .replace(/[^a-zA-Z0-9-]/g, "");
-  const newPost = new Post({
+  const newCase = new Case({
     ...req.body,
     slug,
     userId: req.user.id,
   });
   try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    const savedCase = await newCase.save();
+    res.status(200).json(savedCase);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getcasetemplates = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const cases = await Case.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.caseId && { _id: req.query.caseId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updateAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalCases = await Case.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthCases = await Case.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({
+      cases,
+      totalCases,
+      lastMonthCases,
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema(
+const caseSchema = new mongoose.Schema(
   {
     userId: {
       type: String,
@@ -33,6 +33,6 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Post = mongoose.model("Post", postSchema);
+const Case = mongoose.model("Case", caseSchema);
 
-export default Post;
+export default Case;
